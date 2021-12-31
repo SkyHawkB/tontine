1495762894da5963c50454c7792f8a2a153b824e
Kiera M <kierajreed@gmail.com>
2021-12-31 14:05:06 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo apt install firefox firefox-geckodriver
 mkdir tontine && cd tontine
-https://raw.githubusercontent.com/skyhawkb/tontine/main/tontine
+wget https://raw.githubusercontent.com/skyhawkb/tontine/main/tontine
 chmod +x tontine
 echo "EMAIL: $1" >> creds.txt
 echo "PASSWORD: $2" >> creds.txt
